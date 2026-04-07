50ef837d7b932f8f982c23b9ae066f2e11b29c5a
MMunim <mmunim9.9.01@gmail.com>
2026-04-07 22:26:32 +0600

    operator in python

---
+++ Module 01/operators.py
@@ -0,0 +1,13 @@
+# +, -, *, /, %, //
+
+num1 = 45
+num2 = 5
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+print(10/3) # 3.3333333333333335
+print(10%3) # 1
+print(10//3) # 3 
