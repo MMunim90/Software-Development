7bcc66567f579a236cf1e9c81b2b6512e6f0ce90
MMunim <mmunim9.9.01@gmail.com>
2026-04-11 00:28:47 +0600

    built in function in python

---
+++ Module 02/built_in_function.py
@@ -0,0 +1,15 @@
+# highest = max(45, 98, 46, 87, 96, 41)
+highest = max([45, 98, 46, 87, 96, 41])
+print(highest)
+
+smallest = min([45, 98, 46, 87, 96, 41])
+print(smallest)
+
+# print(min)
+
+count = len([34, 43, 46])
+print(count)
+
+
+total = sum([45, 98, 46, 87, 96, 41])
+print(total)
