b99e3c2b6a049e0a3c14e7e3b63104209264b19b
MMunim <mmunim9.9.01@gmail.com>
2026-04-11 00:58:23 +0600

    modules in function in python

---
+++ Module 02/modules.py
@@ -0,0 +1,9 @@
+from function import double_it
+from kargs_multiple import full_name as name
+from default_args import *
+
+result = double_it(33)
+print(result)
+
+f_name = name('Al', 'Munim')
+print(f_name)
