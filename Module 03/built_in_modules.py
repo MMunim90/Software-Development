b651f1d7807258475362d909fd03ad45e242f123
MMunim <mmunim9.9.01@gmail.com>
2026-04-12 18:51:20 +0600

    built in modules in pyhton

---
+++ Module 03/built_in_modules.py
@@ -0,0 +1,17 @@
+from math import *
+# result = ceil(4.6)
+# result = ceil(4.0000000001)
+# result = floor(4.0000000001)
+# result = floor(4.99999999999)
+# print(result)
+
+
+from random import *
+# print(random())
+# print(randint(1, 100))
+# print(choice(['sakib', 'mash', 'mushi', 'riad', 'musta', 'tamim', 'liton', 'taskin', 'mehedi']))
+
+
+from time import sleep
+sleep(3)
+print(choice(['sakib', 'mash', 'mushi', 'riad', 'musta', 'tamim', 'liton', 'taskin', 'mehedi']))
