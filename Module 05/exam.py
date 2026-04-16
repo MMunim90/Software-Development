2b8ab51b0c620c370a7719055d451ef7edf34945
MMunim <mmunim9.9.01@gmail.com>
2026-04-16 22:57:46 +0600

    make exam related basic app using class

---
+++ Module 05/exam.py
@@ -0,0 +1,36 @@
+from random import *
+class Exam:
+    def __init__(self, examinee):
+        self.examinee = examinee
+        
+    def attend_exam(self):
+        print('Welcome for attending an exam!!!')
+        print('Which subject exam do you want to participate? ')
+        self.exam_subjects()
+        sub = input()
+        print('Choose your exam duration: ')
+        self.available_exam_time()
+        time = input()
+        print('Wow nice! Seems like your preparation is very good? Ok best of luck. Your time start now....')
+        print(f'Subject: {sub} and Time: {time}')
+    
+    def exam_subjects(self):
+        subjects = ['Bangla', 'English', 'Math', 'Physics', 'Chemistry', 'Biology']
+        for i, sub in enumerate(subjects, start=1):
+            print(i, '->', sub)
+        return subjects
+        
+    def available_exam_time(self):
+        time = ['2 hrs', '1 hrs', '30 min', '15 min', '5 min', '1 min']
+        for i, t in enumerate(time, start=1):
+            print(i, '->', t)
+        return time
+    
+    def get_marks(self):
+        print(f'Wow!!! You did outstanding. Your obtained mark\'s is: {randint(70, 100)}')
+        
+munim = Exam('munim')
+# munim.exam_subjects()
+# munim.available_exam_time()
+# munim.attend_exam()
+munim.get_marks()
