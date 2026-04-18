613a26030fcd85aea85a6c7ace4af0f49549dd67
MMunim <mmunim9.9.01@gmail.com>
2026-04-18 21:53:24 +0600

    inheritance concepts

---
+++ Module 06/inheritance_concepts.py
@@ -0,0 +1,13 @@
+# base class, parent class
+class BaseClass:
+    pass
+
+# derived class or child class
+class DerivedClass(BaseClass):
+    pass
+
+""" 
+1. simple inheritance: parent class ---> child class (Device ---> Phone) (Device ---> Laptop)
+
+2. Multi-level inheritance: Grandpa ---> Parent ---> child (Vehicle ---> Bus ---> AC Bus)
+"""
